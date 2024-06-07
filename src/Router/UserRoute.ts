5887ce44c89e335e1d45b3045740413c5bb9f4b6
Pasvan <141811917+Pasvan-arul@users.noreply.github.com>
2024-06-07 17:48:22 +0530

    updated route file

--- src/Router/UserRoute.ts
+++ src/Router/UserRoute.ts
@@ -7,3 +7,14 @@ export const UserRoute = Router();
 UserRoute.use(Token.userToken);
 
 UserRoute.get("/:id/plan", Use(UserController.planGet));
+UserRoute.post("/:id/plan", Use(UserController.planSubscribe));
+UserRoute.get("/:id/video", Use(UserController.listVideo));
+UserRoute.post("/:id/video", Use(UserController.addFavVideo));
+UserRoute.get("/:id/fav_video", Use(UserController.listFavVideo));
+UserRoute.delete("/:id/fav_video", Use(UserController.removeFavVideo));
+UserRoute.post("/:id/group", Use(UserController.createGroup));
+
+UserRoute.get("/:id/group", Use(UserController.listGroup));
+UserRoute.get("/:id/member_group", Use(UserController.addGroupMember));
+UserRoute.delete("/:id/member_group", Use(UserController.deleteGroupMember));
+UserRoute.delete("/:id/group", Use(UserController.deleteGroup));
